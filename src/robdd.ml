9c800b8140c2523db3cd924e26302c4b5a3b5801
Scott Murphy <scottmurphy09@gmail.com>
2017-12-27 18:12:07 -0600

    Added some docs and robdd extensions

--- src/robdd.ml
+++ src/robdd.ml
@@ -17,10 +17,8 @@ module Robdd =
     type id = Id of int
     type label = Label of string
 
-    (* The bdd is more complex because it carries extra tags to make it easier to reduce a binary function *)                    
 
-    type 'a opt  = None| Some of 'a
-
+    (* map over some data *) 
     let mapSome f b  = match b with
       | None  -> b
       | Some a -> Some (f a)
@@ -30,18 +28,46 @@ module Robdd =
                      value: bool;}
                  
     type bdd = { node: bddData;
-                 zeroChild: (bdd opt);
-                 oneChild: bdd opt  }
+                 zeroChild: (bdd option);
+                 oneChild: bdd option  }
 
+                
+    (* standard map over data *)
+    let rec map f b = { node = f b.node;
+                        zeroChild = mapSome (map f) b.zeroChild;
+                        oneChild = mapSome (map f) b.oneChild;
+                      }
+
+
+    (* fold with right most bias*)                    
+    let rec fold f b bdd : (bool -> 'a -> 'a) -> 'a -> bdd -> 'a =
+      let rsltV = f bdd.node.value b
+      in match bdd.zeroChild with
+           None -> rsltV 
+         | Some bddZ -> let rsltZ = (fold f rsltV bddZ)
+                        in match bdd.oneChild with
+                             None -> rsltZ
+                           | Some bddO -> fold f rsltZ bddO
+
+                                        
+                                        
 
     (* store a list of names with repitition handled by giving each one its own number
        ("x",i) for instance where
-       i.1, i.2, i.3 ... Only the last one is stored, this only provides fresh names  *)
+       i.1, i.2, i.3 ... Only the last one is stored, this only provides fresh names  
 
-             
+       The below types and functions are to deal with the business of naming and labeling parts of the BDD
+*)
+
+                                        
+                           
+                               
     type names = (string,int) Hashtbl.t               
     type name  = (string * int)
-    
+
+
+    let emptyNames : names = Hashtbl.create 0
+               
 
 
     let addName names (name,i)  = match Hashtbl.find_all names name with
@@ -62,28 +88,12 @@ module Robdd =
         [] -> Hashtbl.add oldNames name 1
        |x::_ -> Hashtbl.replace oldNames name (x+1)
 
-
               
-    let emptyNames : names = Hashtbl.create 0
-                           
-                   
-    let rec map f b = { node = f b.node;
-                        zeroChild = mapSome (map f) b.zeroChild;
-                        oneChild = mapSome (map f) b.oneChild;
-                      }
-                    
-    let rec fold f b bdd : (bool -> 'a -> 'a) -> 'a -> bdd -> 'a =
-      let rsltV = f bdd.node.value b
-      in match bdd.zeroChild with
-           None -> rsltV 
-         | Some bddZ -> let rsltZ = (fold f rsltV bddZ)
-                        in match bdd.oneChild with
-                             None -> rsltZ
-                           | Some bddO -> fold f rsltZ bddO
-
-                                        
-                                        
-
-
+    (*  nbdd are bdd's with the concept of a name space 
+        this allows simple naming for the boolean operations that require labeling *)
+    type nbdd = { name:string;
+                  bdd:bdd}
+              
+                
 
   end
