609668abb61309efc42322d4b9696aa42c5b3784
Scott Murphy <scottmurphy09@gmail.com>
2018-01-12 16:10:27 -0600

    dropped distinction between names and labels
    When I first did this I felt like labels needed to be separated
    from a more solid node identifier.  This was wrong

--- src/robdd.ml
+++ src/robdd.ml
@@ -50,12 +50,11 @@ module Robdd =
 
 
 
-
     (* BDD Stuff *)
 
     (* Data type for BDD *)
               
-    type id = Id of name
+
     type label = Label of name
     type 'a noption = NSome of 'a | Zero | One
 
@@ -68,19 +67,15 @@ module Robdd =
 
                 
 
-    type bddData = { id : id;
-                    label: label}
+    type bddData = { label: label}
     
-    let zeroNode = {id= Id ("zero",0) ; label = Label ("zero",0)}
-    let oneNode = {id= Id ("one",1) ; label = Label ("one",1)}
+    let zeroNode = {label = Label ("zero",0)}
+    let oneNode = {label = Label ("one",1)}
                  
 
-    let compareBddData bddA bddB =  let (Id nameIdA) = bddA.id
-                                    and (Label nameLabelA) = bddA.label
-                                    and (Id nameIdB) = bddB.id
+    let compareBddData bddA bddB =  let (Label nameLabelA) = bddA.label
                                     and (Label nameLabelB) = bddB.label
-                                    in (nameIdA == nameIdB) &&
-                                       (nameLabelA == nameLabelB) 
+                                    in (nameLabelA == nameLabelB) 
                                                                 
 
 
@@ -124,8 +119,7 @@ module Robdd =
                            | NSome bddO -> fold f rsltZ bddO
 
                                         
-    let mk name z o = {node={id=(Id name);
-                             label=(Label name)};
+    let mk name z o = {node={label=(Label name)};
                        zeroChild = z;
                        oneChild = o}
 
@@ -134,13 +128,12 @@ module Robdd =
                       
     let gvBddNode n =
       let (Label l) = n.label
-         in let (Id i) = n.id
-            in (showName i) ^ " [label=\"" ^ (showName l) ^ "\"" ^ "];"
+         in (showName l) ^ " [label=\"" ^ (showName l) ^ "\"" ^ "];"
 
 
 
     let gvBddNodeEdgeLabel n =
-      let (Id i) = n.id
+      let (Label i) = n.label
       in (showName i) 
 
     let gvBddNodeEdge style n1 n2 = (gvBddNodeEdgeLabel n1) ^ " -> " ^ (gvBddNodeEdgeLabel n2) ^ " [style="^style^"];"
@@ -196,7 +189,89 @@ module Robdd =
             in  (preamble ^ nodeDefinitions ^ graphStart ^ zeroDefinitions ^ oneDefinitions ^ "}}")
 
 
-       
+
+              
+    (* apply a binary function in two arguments to a bdd *) 
+(* From Logic in Computer Science
+Given two diagrams B_f and B_g considering nodes r_f and r_g
+(0|1) (0|1) case
+1. If both r f and r g are terminal nodes with labels l f and l g , respectively (recall
+   that terminal labels are either 0 or 1), then we compute the value l f op l g and
+   let the resulting OBDD be B 0 if that value is 0 and B 1 otherwise.
+
+X_i X_i case
+2. In the remaining cases, at least one of the root nodes is a non-terminal. Suppose
+   that both root nodes are x i -nodes. Then we create an x i -node n with a dashed
+   line to apply (op, lo(r f ), lo(r g )) and a solid line to apply (op, hi(r f ), hi(r g )), i.e.
+   we call apply recursively on the basis of (6.2).
+
+X_i (X_j : j>i | 0 | 1) case
+3. If r f is an x i -node, but r g is a terminal node or an x j -node with j > i,
+   then we know that there is no x i -node in B g because the two OBDDs have
+   a compatible ordering of boolean variables. Thus, g is independent of x i
+   (g ≡ g[0/x i ] ≡ g[1/x i ]). Therefore, we create an x i -node n with a dashed line
+   to apply (op, lo(r f ), r g ) and a solid line to apply (op, hi(r f ), r g ).
+
+X_j (X_i : i>j | 0 | 1) case
+4. The case in which r g is a non-terminal, but r f is a terminal or an x j -node with
+   j > i, is handled symmetrically to case 3.
+
+ *)
+
+
+
+
+    let boolAsOptional b = if b 
+                           then Zero
+                           else One
+
+                                       
+    (* Unlike apply above, this is for terminal application only 
+       So the behavior in the case that it is applied to a non-terminal node is 
+       arbitrary *) 
+    let applyArgument noptBddA noptBddB op =
+      (match (noptBddA,noptBddB) with
+         (Zero, Zero) -> boolAsOptional (op false false)
+       | (Zero, One)  -> boolAsOptional (op false true)
+       | (One , Zero) -> boolAsOptional (op true false)
+       | (One ,One)   -> boolAsOptional (op true true)                                                               
+       | (a,_) -> a )
+      
+
+
+                                           
+    let rec apply noptBddA noptBddB op =
+      (match (noptBddA,noptBddB) with
+         (Zero,Zero) -> applyArgument Zero Zero op
+       | (Zero,One)  -> applyArgument Zero One  op
+       | (One ,Zero) -> applyArgument One  Zero op                      
+       | (One ,One ) -> applyArgument One  One  op                 
+       | (NSome rf, NSome rg) when (rf.node == rg.node) ->
+          (NSome ({node = rf.node;
+                   zeroChild = (apply rf.zeroChild rg.zeroChild op);
+                   oneChild  = (apply rf.oneChild  rg.oneChild op)}))
+
+       | (NSome rf, NSome rg) when (rf.node != rg.node) ->
+          (NSome ({node = rf.node;
+                   zeroChild = (apply rf.zeroChild rg.zeroChild op);
+                   oneChild  = (apply rf.oneChild  rg.oneChild op)}))
+
+                  
+       | (NSome rf, Zero) ->
+          (NSome ({node = rf.node;
+                   zeroChild = (apply rf.zeroChild Zero op);
+                   oneChild  = (apply rf.oneChild  Zero op)}))
+       | (NSome rf, One) ->
+          (NSome ({node = rf.node;
+                   zeroChild = (apply rf.zeroChild One op);
+                   oneChild  = (apply rf.oneChild  One op)}))
+       | (NSome _, NSome _) -> failwith "guarded condition should have caught apply"
+      )
+
+
+
+   
+                                           
     (* 
        The below types and functions are to deal with the business of naming and labeling parts of the BDD
      *)                                        
@@ -207,7 +282,8 @@ module Robdd =
     type nbdd = { name:name;
                   bdd:bdd}
 
-
+    
+                 
     (* Example graphs for testing *)            
 
     let exBf = let    x4 = mk ("x",4) Zero One
@@ -215,10 +291,14 @@ module Robdd =
                   in let x2 = mk ("x",2) (NSome x4) (NSome x3)
                      in mk ("x",1) (NSome x2) (NSome x3)
 
-    let exBg = let x4 = mk ( "x",4) One Zero
+    let exBg = let    x4 = mk ( "x",4) Zero One
                in let x3 = mk ( "x",3) (NSome x4) One
                   in  mk ( "x",1) (NSome x4) (NSome x3)
-                    
+
+
+    let exBgBf = apply (NSome exBf) (NSome exBg) (||)
+    let prop_reflexive_apply = apply One One (||) 
+                             
     let exSimple = let x3 = mk ("x",3) Zero One
                    in mk ("x",1) (NSome x3) (NSome x3)
                     
